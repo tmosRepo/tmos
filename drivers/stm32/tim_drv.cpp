7c56a747248cb1b2e28d3d0165150c60c0c9917e
bratkov <s.bratkov@eltrade.com>
2024-05-19 16:58:43 +0300

    uses the global variables for the APB clocks

--- drivers/stm32/tim_drv.cpp
+++ drivers/stm32/tim_drv.cpp
@@ -11,16 +11,21 @@
 static void ConfigureTimer(TIM_DRV_INF drv_info, const TIMER_CONTROL_MODE *mode)
 {
 	TIM_TypeDef* hw_base = drv_info->hw_tmr_base;
-	RCC_ClocksTypeDef  clocks;
-
+	uint32_t frequency;
 
 	hw_base->TIM_CR1 = mode->cr1;
 	hw_base->TIM_CR2 = mode->cr2;
 	hw_base->TIM_ARR = mode->arr_or_ccr;
-	// The rescaler values refer to a frequency of 120 or 60 MHz
-	// depending on which peripheral the timer is connected to,
-	// If the current frequency is different, the prescaler must be recalculated.
-	RCC_GetClocksFreq (&clocks);
+
+	// If the APB prescaler is 1, the timer clock frequencies are set to the same frequency as
+	// that of the APB domain to which the timers are connected. Otherwise, they are set to twice (x2)
+	// the frequency of the APB domain to which the timers are connected.
+
+	// NOTE: some F4 series have a register RCC_DCKCFGR which has TIMPRE bit.
+	// If it set and the APB prescaler (PPRE1, PPRE2 in the RCC_CFGR register) is configured to a
+	// division factor of 1, 2 or 4, TIMxCLK = HCLK. Otherwise, the timer clock frequencies are set
+	// to four times to the frequency of the APB domain to which the timers are connected:TIMxCLK = 4xPCLKx.
+
 	switch(drv_info->info.peripheral_indx)
 	{
 	case ID_PERIPH_TIM1:
@@ -29,13 +34,21 @@ static void ConfigureTimer(TIM_DRV_INF drv_info, const TIMER_CONTROL_MODE *mode)
 	case ID_PERIPH_TIM10:
 	case ID_PERIPH_TIM11:
 		// APB2 120(2x60)
-		clocks.PCLK2_Frequency /=500000;
-		hw_base->TIM_PSC = ((uint32_t)(mode->psc)*clocks.PCLK2_Frequency)/120;
+		if((RCC_CFGR_PPRE2 & RCC->RCC_CFGR) == RCC_CFGR_PPRE2_DIV1){
+			frequency = APB2_clock_frequency /1000;
+		}else{
+			frequency = APB2_clock_frequency /500;
+		}
+		hw_base->TIM_PSC = frequency/mode->fclk;
 		break;
 	default:
 		// APB1 60(2x30)
-		clocks.PCLK1_Frequency /=500000;
-		hw_base->TIM_PSC = ((uint32_t)(mode->psc)*clocks.PCLK1_Frequency)/60;
+		if((RCC_CFGR_PPRE1 & RCC->RCC_CFGR) == RCC_CFGR_PPRE1_DIV1){
+			frequency = APB1_clock_frequency /1000;
+		}else{
+			frequency = APB1_clock_frequency /500;
+		}
+		hw_base->TIM_PSC = frequency/mode->fclk;
 		break;
 	}
 	hw_base->TIM_RCR = mode->rcr;
